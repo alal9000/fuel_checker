8aa2a57c6341b9ac7591d4b0bb1e4025081c5636
Aaron <aaronlal72@gmail.com>
2022-09-11 14:53:27 +1000

    latest

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class TrackerForm(forms.Form):
-    text = forms.CharField(max_length=40, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter todo", 'aria-label': 'Todo', 'aria-describedby':'add-btn'}))
+    text = forms.CharField(max_length=40, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Station @ price", 'aria-label': 'Todo', 'aria-describedby':'add-btn'}))
